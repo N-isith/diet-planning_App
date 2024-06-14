e49e716bdea715e7f4be77bac0f34cc2a2828786
N-isith <nisithalakshan94@gmail.com>
2024-06-14 22:07:01 +0530

    login session added. also some styles

--- backend/signin.php
+++ backend/signin.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include("config.php");
 
 if(isset($_POST["signinsubmit"])) {
@@ -14,23 +15,34 @@ if(isset($_POST["signinsubmit"])) {
 
     if($result->num_rows > 0) {
         $row = $result->fetch_assoc();
+        $username = $row['username'];
         $hashedPassword = $row['password'];
         $role = $row['role'];
 
+        //PHP session verififcation
+        if($_POST["userName"] == $username && $_POST["password"] == $hashedPassword){
+            $_SESSION["userName"] = $_POST["userName"];            
+        }
+
+        if(isset($_SESSION["userName"])){
+            
         // Verify the password
         if(password_verify($password, $hashedPassword)) {
             // Redirect to the relevant dashboard
             if($role == 'Dietician') {
-                echo "<script>alert('Sign in successful. Redirecting to Dietician Dashboard.');</script>";
+                echo "<script>alert('Sign in successful. Redirecting to Dietician Dashboard. User: ".$_SESSION["userName"]."');</script>";
                 echo "<script>window.location.href = 'DietDashboard.html';</script>";
             }
             elseif($role == 'Patient') {
-                echo "<script>alert('Sign in successful. Redirecting to Patient Dashboard.');</script>";
+                echo "<script>alert('Sign in successful. Redirecting to Patient Dashboard. User: ".$_SESSION["userName"]." ge session ek');</script>";
                 echo "<script>window.location.href = 'PatientDashboard.html';</script>";
             }
         } else {
             echo "<script>alert('Invalid username or password.'); window.history.back();</script>";
         }
+        }
+
+        
     } else {
         echo "<script>alert('Invalid username or password.'); window.history.back();</script>";
     }
